f8a17335f4314dd6cb49cf4d5c0a74d67d062191
Lucas Farris <lfarris@softserveinc.com>
2017-09-15 11:27:12 +0200

    iOS 11 open spreadsheet view

--- Framework/Sources/SpreadsheetView.swift
+++ Framework/Sources/SpreadsheetView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class SpreadsheetView: UIView {
+open class SpreadsheetView: UIView {
     /// The object that provides the data for the collection view.
     ///
     /// - Note: The data source must adopt the `SpreadsheetViewDataSource` protocol.
@@ -138,7 +138,7 @@ public class SpreadsheetView: UIView {
 
     #if swift(>=3.2)
     @available(iOS 11.0, *)
-    public override func safeAreaInsetsDidChange() {
+    open override func safeAreaInsetsDidChange() {
         if let backgroundView = backgroundView {
             backgroundView.removeFromSuperview()
             super.insertSubview(backgroundView, at: 0)
